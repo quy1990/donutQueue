03b85fbdbcebd376e4cdfed782bf624e0b79b2e1
tam@gmail.com <tam@gmail.com>
2021-05-23 23:45:31 +0200

    Added test

--- atrify.sql
+++ atrify.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: database
--- Thời gian đã tạo: Th5 23, 2021 lúc 02:14 PM
+-- Thời gian đã tạo: Th5 23, 2021 lúc 09:35 PM
 -- Phiên bản máy phục vụ: 8.0.25
 -- Phiên bản PHP: 7.4.16
 
@@ -56,42 +56,16 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `client_id`, `quantity`, `created_date`) VALUES
-(0, 0, 1, '2021-05-20 20:37:03'),
-(1, 1, 1, '2021-05-16 10:35:56'),
-(2, 2, 1, '2021-05-16 10:36:04'),
-(3, 3, 1, '2021-05-16 10:37:50'),
-(4, 4, 1, '2021-05-16 10:43:06'),
-(5, 5, 1, '2021-05-16 11:19:56'),
-(6, 6, 1, '2021-05-16 11:21:58'),
-(7, 7, 1, '2021-05-16 12:19:55'),
-(8, 8, 1, '2021-05-16 12:20:00'),
-(9, 9, 1, '2021-05-16 12:21:15'),
-(10, 10, 1, '2021-05-22 21:42:57'),
-(11, 11, 1, '2021-05-23 13:44:47'),
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `products`
---
-
-CREATE TABLE `products` (
-  `id` bigint NOT NULL,
-  `image_url` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Đang đổ dữ liệu cho bảng `products`
---
-
-INSERT INTO `products` (`id`, `image_url`, `name`) VALUES
-(145, 'https://cphapp.rema1000.dk/api/v1/catalog/store/1/item/23420/image/1462221519/600.jpg', 'CREMEFRAICHE'),
-(146, 'https://cphapp.rema1000.dk/api/v1/catalog/store/1/item/213942/image/1560262700/600.jpg', 'PEPSI MAX'),
-(147, 'https://cphapp.rema1000.dk/api/v1/catalog/store/1/item/201003/image/1553075410/600.jpg', 'CARLSBERG'),
-(148, 'https://cphapp.rema1000.dk/api/v1/catalog/store/1/item/400176/image/1569941273/600.jpg', 'HK. OKSEKÃ˜D 15-18'),
-(149, 'https://cphapp.rema1000.dk/api/v1/catalog/store/1/item/212652/image/1549284118/600.jpg', 'COCA COLA'),
-(150, 'https://cphapp.rema1000.dk/api/v1/catalog/store/1/item/61250/image/1534406146/600.jpg', 'BURGERBOLLER');
+(1, 1, 1, '2021-05-23 21:33:05'),
+(2, 2, 1, '2021-05-23 21:33:50'),
+(3, 3, 1, '2021-05-23 21:33:50'),
+(4, 4, 1, '2021-05-23 21:33:50'),
+(5, 5, 1, '2021-05-23 21:33:50'),
+(6, 6, 1, '2021-05-23 21:33:50'),
+(7, 7, 1, '2021-05-23 21:33:50'),
+(8, 8, 1, '2021-05-23 21:33:50'),
+(9, 9, 1, '2021-05-23 21:33:50'),
+(10, 10, 1, '2021-05-23 21:33:50');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -101,13 +75,6 @@ INSERT INTO `products` (`id`, `image_url`, `name`) VALUES
 -- Chỉ mục cho bảng `orders`
 --
 ALTER TABLE `orders`
-  ADD UNIQUE KEY `client_id` (`id`),
-  ADD UNIQUE KEY `client_id_2` (`client_id`);
-
---
--- Chỉ mục cho bảng `products`
---
-ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -118,13 +85,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT cho bảng `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
-
---
--- AUTO_INCREMENT cho bảng `products`
---
-ALTER TABLE `products`
-  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=151;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
